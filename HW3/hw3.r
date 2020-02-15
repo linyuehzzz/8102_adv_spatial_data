17b00d94d2f1f3a01f11f4a84ef7ffcb23a13d5f
Lin, Yue <lin.3326@osu.edu>
2020-02-15 16:00:22 -0500

    add files for hw3

--- HW3/hw3.r
+++ HW3/hw3.r
@@ -141,3 +141,99 @@ tl.1 = expression(paste("Kernel Estimation of RW: ", sigma, " = 0.1"))
 plot(rw.Z.1, main=tl.1)
 plot(rw.Z.1, main="Kernel Estimation of RW: sigma = 0.1")
 points(rw, pch="+", col="6")
+
+## nearest neighbor distances (event-to-nearest-event distances)
+rw.ghat = Gest(rw)
+g.max = max(rw.ghat$r)
+plot(rw.ghat, cbind(rs,theo)~r, main="G Estimates", 
+     xlab="r", ylab="G(r)")
+
+## nearest neighbor distances (point-to-nearest-event distances)
+rw.fhat = Fest(rw)
+f.max = max(rw.fhat$r)
+plot(rw.fhat, cbind(rs,theo)~r, main="F Estimates",
+     xlab="r", ylab="F(r)")
+
+## K function
+?Kest
+rw.khat = Kest(rw)
+plot(rw.khat, cbind(border, theo)~r, main="K function for RW")
+
+## L function
+plot(rw.khat, sqrt(border/pi)-r ~ r, ylab="L(r)",
+     main="L function for RW")
+abline(h=0, lty=2, col="red")
+
+## test for CSR with simulations (Fhat)
+rw.fhat = Fest(rw)
+rw.win = window(rw)
+rw.fenv = fhat.env(n=rw$n, s=100, r=rw.fhat$r, rw.win)
+plot(rw.fhat, rs~r, main="F estimates")
+lines(rw.fhat$r, rw.fenv$Up, lty=5, col=2)
+lines(rw.fhat$r, rw.fenv$Down, lty=5, col=3)
+
+## test for CSR with simulations (Khat)
+rw.khat = Kest(rw)
+rw.win = window(rw)
+rw.kenv = khat.env(n=rw$n, s=100, r=rw.khat$r, rw.win)
+plot(rw.khat, border~r, main="K function for RW")
+lines(rw.khat$r, rw.kenv$Up, lty=5, col=2)
+lines(rw.khat$r, rw.kenv$Down, lty=5, col=3)
+
+# 4.2 Regular points
+rp = rsyst(nx=10) #generate regular pattern
+summary(rp)
+
+## kernel estimation (sigma = 0.05)
+?density.ppp
+rp.Z.05 = density.ppp(rp, 0.05)
+par(mar=c(0,0,1,1)) # set plot margin
+tl.05 = expression(paste("Kernel Estimation of RP: ", sigma, " = 0.05"))
+plot(rp.Z.05, main = tl.05)
+plot(rp.Z.05, main="Kernel Estimation of RP: sigma = 0.05")
+points(rp, pch="+", col="6")
+
+## kernel estimation (sigma = 0.1)
+rp.Z.1 = density.ppp(rp, 0.1)
+tl.1 = expression(paste("Kernel Estimation of RP: ", sigma, " = 0.1"))
+plot(rp.Z.1, main=tl.1)
+plot(rp.Z.1, main="Kernel Estimation of RP: sigma = 0.1")
+points(rp, pch="+", col="6")
+
+## nearest neighbor distances (event-to-nearest-event distances)
+rp.ghat = Gest(rp)
+g.max = max(rp.ghat$r)
+plot(rp.ghat, cbind(rs,theo)~r, main="G Estimates", 
+     xlab="r", ylab="G(r)")
+
+## nearest neighbor distances (point-to-nearest-event distances)
+rp.fhat = Fest(rp)
+f.max = max(rp.fhat$r)
+plot(rp.fhat, cbind(rs,theo)~r, main="F Estimates",
+     xlab="r", ylab="F(r)")
+
+## K function
+?Kest
+rp.khat = Kest(rp)
+plot(rp.khat, cbind(border, theo)~r, main="K function for RP")
+
+## L function
+plot(rp.khat, sqrt(border/pi)-r ~ r, ylab="L(r)",
+     main="L function for RP")
+abline(h=0, lty=2, col="red")
+
+## test for CSR with simulations (Fhat)
+rp.fhat = Fest(rp)
+rp.win = window(rp)
+rp.fenv = fhat.env(n=rp$n, s=100, r=rp.fhat$r, rp.win)
+plot(rp.fhat, rs~r, main="F estimates")
+lines(rp.fhat$r, rp.fenv$Up, lty=5, col=2)
+lines(rp.fhat$r, rp.fenv$Down, lty=5, col=3)
+
+## test for CSR with simulations (Khat)
+rp.khat = Kest(rp)
+rp.win = window(rp)
+rp.kenv = khat.env(n=rp$n, s=100, r=rp.khat$r, rp.win)
+plot(rp.khat, border~r, main="K function for RP")
+lines(rp.khat$r, rp.kenv$Up, lty=5, col=2)
+lines(rp.khat$r, rp.kenv$Down, lty=5, col=3)
