d8e5fbc752484a992eb4f7bdc2630250691cb7cb
Lin, Yue <lin.3326@osu.edu>
2020-02-26 08:33:13 -0500

    hw4

---
+++ HW4/hw4.r
@@ -0,0 +1,85 @@
+# 1. The GeoR package
+library(geoR)
+library(maps)
+library(mvtnorm) # package for simulation
+rm(list=ls()) # clear workspace
+
+# 2. Simulating a Gaussian spatial process
+# 2.1 Obtain the limits of our spatial domain
+ia.range = map("state", "iowa", plot=FALSE)$range
+rx = ia.range[2] - ia.range[1] # range in x direction
+ry = ia.range[4] - ia.range[3] # range in y direction
+
+# 2.2 Define the spatial points (locations)
+xg = seq(ia.range[1], ia.range[2], length.out=30)
+yg = seq(ia.range[3], ia.range[4], length.out=30)
+iagrid.locs = expand.grid(xg, yg)
+map("state", "iowa")
+points(iagrid.locs, pch=20)
+ntot = dim(iagrid.locs)[1]
+
+# 2.3 Get a matrix of pairwise distances
+distmat = as.matrix(dist(iagrid.locs))
+max_dist = max(distmat)
+
+# 2.4 Use a true exponential covariance model
+theta1 = 5
+theta2 = 0.5
+Sig = theta1 * exp(-distmat / theta2)
+plot(seq(0, max_dist, length.out=20), 
+     theta1 * exp(-seq(0, max_dist, length.out=20) / theta2),
+     type="b", ylab="cov", xlab="distance")
+
+# 2.5 Simulate a spatial random process from the Gaussian model
+y = rmvnorm(1, matrix(5,ntot,1), Sig)
+image(matrix(y, length(xg), length(yg)), x=xg, y=yg,
+        col=rev(rainbow(100, start=0, end=.7)))
+map("state", "iowa", lwd=3, add=TRUE)
+box() # add a boundary box to the plot
+
+# 3. Use a subset of the simulated process as working data
+idxkeep = sort(sample(1:ntot, round(0.3*ntot)))
+ymask = matrix(0, ntot, 1)
+ymask[idxkeep, ] = 1
+image(matrix(y, length(xg), length(yg)), x=xg, y=yg,
+      col = rev(rainbow(100,start=0,end=.7)))
+image(matrix(ymask, length(xg), length(yg)), x=xg, y=yg,
+      col=c("white","transparent"), add=TRUE)
+map("state", "iowa", add=TRUE, lwd=3)
+box()
+
+# 4. Estimate the empirical semi-variogram
+# 4.1 Create a geodata object using the subset of simulated data
+n = length(idxkeep)
+ydat = matrix(y[idxkeep], n, 1)
+dat.locs = iagrid.locs[idxkeep, ]
+ygeodat = as.geodata(cbind(dat.locs, ydat))
+
+# 4.2 Estimate the empirical semi-variogram of the sample data
+distsample = as.matrix(dist(dat.locs)) # distance between sample pairs
+maxd = max(distsample)/2
+y.v = variog(ygeodat, max.dist=maxd) # estimate empirical semi-variogram
+y.v$beta.ols # estimated trend of the sample data
+
+# 4.3 Estimate the true semi-variogram
+vt = theta1 - theta1 * exp(-y.v$u/theta2)
+
+# 4.4 Plot the empirical semi-variogram and true semi-variogram
+xmax = max(y.v$u, maxd) # xmax to set xlim for plot
+ymax = max(y.v$v, vt) # ymax to set ylim for plot
+plot(y.v, xlim = c(0,xmax), ylim = c(0,ymax), col="red") # empricial semivariogram
+lines(y.v$u, vt, type="b", pch=20, col="blue", lty=2) # add true semivariogram
+legend("bottomright", legend=c("Empricial", "True"), col=c("red", "blue"), lty=1:2, cex=0.8)
+
+# 4.5 Plot the empirical semi-variogram in each of 4 principle directions
+plot(variog4(ygeodat, max.dist=maxd))
+
+
+# 5. Fit an exponential semi-variogram model 
+y.v.wls=variofit(y.v, cov.model="exponential", wei="cressie")
+y.v.wls
+plot(y.v ,xlim=c(0,xmax), ylim=c(0,ymax), col="red") # empirical semivariogram
+lines(y.v.wls, col="black", lty=2) # fitted semivariogram
+lines(y.v$u,vt, type="b", pch=20, col="blue", lty=3) # true semivariogram
+legend("bottomright", legend=c("Empricial", "Fitted", "True"), 
+       col=c("red", "black", "blue"), lty=1:3, cex=0.8)
